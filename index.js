720d2e8c02f3163927264043773652c0563feb79
tomasotano25 <tomasotano25@gmail.com>
2022-09-26 13:01:01 -0400

    Clase 06 get: recibir parametros

--- index.js
+++ index.js
@@ -1,24 +1,61 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/' , (req, res) => {
-  res.send("Hola mi server en express")
-})
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
 
-app.get('/nueva-ruta' , (req, res) => {
-  res.send("Hola, soy una nueva ruta")
-})
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
 
-app.get('/products' , (req, res) => {
+app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 2000,
+    },
+  ]);
+});
+
+// endpoints: simple
+app.get('/products/:productId', (req, res) => {
+  const { productId } = req.params;
+
   res.json({
-    name: "Product 1",
-    price: 1000
-  })
-})
+    id: productId,
+    name: 'Product 1',
+    price: 1000,
+  });
+});
+
+// endpoints: complejos
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+
+  res.json({
+    categoryId,
+    productId,
+  });
+});
+
+// Detalles de producto
+app.get('/products/:id/details', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    id,
+    name: 'Product 1',
+    price: 1000,
+    description: 'Este es mi primer producto',
+  });
+});
 
 app.listen(port, () => {
-  console.log('Mi port: ' + port)
-})
-
-
+  console.log('Mi port: ' + port);
+});
