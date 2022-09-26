1e33586f8925bcf9ad74d86085fb4bee507143d0
tomasotano25 <tomasotano25@gmail.com>
2022-09-26 16:35:32 -0400

    Clase 12 codigos de estados

--- routes/products.router.js
+++ routes/products.router.js
@@ -20,16 +20,22 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   const { id } = req.params;
 
-  res.json({
-    id,
-    name: 'Product 1',
-    price: 1000,
-  });
+  if (id === '999') {
+    res.status(404).json({
+      message: 'not found',
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name: 'Product 1',
+      price: 1000,
+    });
+  }
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body,
   });
